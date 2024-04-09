ec58f3c38ffb17655dd18f0861808c7705a7e6ec
nafisk <nafisrizwank@gmail.com>
2024-04-09 15:35:08 -0400

    add counter

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,14 +1,18 @@
+import { Children, ReactNode } from 'react';
 
+type CounterProps = {
+  setCount: React.Dispatch<React.SetStateAction<number>>;
+  children: ReactNode;
+};
 
-function Counter() {
-
+const Counter = ({ setCount, children }: CounterProps) => {
   return (
     <>
-      <h1>Count is {count}</h1>
+      <h1>{children}</h1>
       <button onClick={() => setCount(prev => prev + 1)}>+</button>
       <button onClick={() => setCount(prev => prev - 1)}>-</button>
     </>
   );
-}
+};
 
 export default Counter;

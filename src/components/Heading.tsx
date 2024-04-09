ed388655033bef02273e1fa0c48efb03ce52d8bf
nafisk <nafisrizwank@gmail.com>
2024-04-09 15:14:38 -0400

    create initial folders

---
+++ src/components/Heading.tsx
@@ -0,0 +1,11 @@
+import { ReactElement } from 'react';
+
+type HeadingProps = {
+  title: string;
+};
+
+const Heading = ({ title }: HeadingProps): ReactElement => {
+  return <div>{title}</div>;
+};
+
+export default Heading;
